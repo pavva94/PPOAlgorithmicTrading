be105d36f540d2342b706b8fa492af84b18e0ba6
Alessandro Pavesi <pavesialessandro@live.it>
2021-08-04 15:52:44 +0200

    added some new arguments

--- main.py
+++ main.py
@@ -23,20 +23,34 @@ if (__name__ == '__main__'):
     parser = argparse.ArgumentParser(description='')
     parser.add_argument("-strategy", default='PPO', type=str, help="Name of the trading strategy")
     parser.add_argument("-stock", default='Apple', type=str, help="Name of the stock (market)")
+    parser.add_argument("-numberOfEpisodes", default=50, type=int, help="Number of training episodes")
+    parser.add_argument("-displayTestbench", default=False, type=bool, help="Dislay Testbench")
+    parser.add_argument("-analyseTimeSeries", default=False, type=bool, help="Start Analysis Time Series")
+    parser.add_argument("-simulateExistingStrategy", default=False, type=bool, help="Start Simulation of an Existing Strategy")
+    parser.add_argument("-evaluateStrategy", default=False, type=bool, help="Start Evaluation of a Strategy")
+    parser.add_argument("-evaluateStock", default=False, type=bool, help="Start Evaluation of a Stock")
     args = parser.parse_args()
 
     # Initialization of the required variables
     simulator = TradingSimulator()
     strategy = args.strategy
     stock = args.stock
+    numberOfEpisodes = args.numberOfEpisodes
+    displayTestbench = args.displayTestbench
+    analyseTimeSeries = args.analyseTimeSeries
+    simulateExistingStrategy = args.simulateExistingStrategy
+    evaluateStrategy = args.evaluateStrategy
+    evaluateStock = args.evaluateStock
 
     # Training and testing of the trading strategy specified for the stock (market) specified
-    simulator.simulateNewStrategy(strategy, stock, saveStrategy=False)
-    """
-    simulator.displayTestbench()
-    simulator.analyseTimeSeries(stock)
-    simulator.simulateNewStrategy(strategy, stock, saveStrategy=False)
-    simulator.simulateExistingStrategy(strategy, stock)
-    simulator.evaluateStrategy(strategy, saveStrategy=False)
-    simulator.evaluateStock(stock)
-    """
+    simulator.simulateNewStrategy(strategy, stock, numberOfEpisodes=numberOfEpisodes, saveStrategy=False)
+    if displayTestbench:
+        simulator.displayTestbench()
+    if analyseTimeSeries:
+        simulator.analyseTimeSeries(stock)
+    if simulateExistingStrategy:
+        simulator.simulateExistingStrategy(strategy, stock)
+    if evaluateStrategy:
+        simulator.evaluateStrategy(strategy, saveStrategy=False)
+    if evaluateStock:
+        simulator.evaluateStock(stock)
